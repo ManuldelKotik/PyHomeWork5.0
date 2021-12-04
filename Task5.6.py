f12d21203d787dd6aaac9ec6992dee6e97388838
Manul <dmitriy-hak@yandex.ru>
2021-12-04 14:14:14 +0300

    Добавлено решение задач к домашнему заданию №5.

---
+++ Task5.6.py
@@ -0,0 +1,9 @@
+mydict = {}
+with open("Task5.6.txt", encoding="utf-8") as f:
+    for line in f:
+        name, stats = line.split(':')
+        elems = [i for i in stats if i == ' ' or (i >= '0' and i <= '9')]
+        print(elems)
+        name_sum = sum(map(int, "".join(elems).split()))
+        mydict[name] = name_sum
+print(f"{mydict}")
